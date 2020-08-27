cad852d6ec3a5a2569371e0c138b370683d5b226
amitravikumar <amitravikumar@users.noreply.github.com>
2020-08-27 01:14:49 +0000

    deploy: b2bb07798f79816cbe812fd43260bd89cf055eea

---
+++ precache-manifest.aff331ae6b4727ad3b8b8b5bc6abd416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db08fec1418fb29be7d01b110e196c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0513bd9d87498887cc71",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "./static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0513bd9d87498887cc71",
+    "url": "./static/js/main.2862e60e.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
