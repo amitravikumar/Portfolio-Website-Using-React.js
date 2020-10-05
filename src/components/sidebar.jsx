d5565333eb299652cbed54195be816109223f4ee
Amit Kumar <besttechamit@gmail.com>
2020-10-05 22:15:20 +0530

    Some content updated

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.png)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(public/images/about.png)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Amit Kumar</a></h1>
               <span className="email"><i className="icon-mail"></i> iamitravikumar@gmail.com</span>
             </div>
